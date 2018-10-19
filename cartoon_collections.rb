bf266366064b5045f7bc3fa8675c41e7134a53ee
Tom Rowson <tom.rowson@gmail.com>
2018-10-19 14:52:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,9 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if 
+  if array.include 
     array.find {|word| word == 
+  else 
+    return NIL
+  end
 end
